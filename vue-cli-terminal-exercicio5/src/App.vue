516d167bf3d500db0624693a8567ea3a54b4d7a8
Everton Cabral <evertoncab@gmail.com>
2021-02-12 14:49:53 -0300

    criação das propriedades computadas e metohds

--- vue-cli-terminal-exercicio5/src/App.vue
+++ vue-cli-terminal-exercicio5/src/App.vue
@@ -1,16 +1,31 @@
 <template>
   <div id="app">
+    <span> fullNameComputed: {{ fullNameComputed }}</span> 
+    <br>
+    <span> fullNameMethods: {{ fullNameMethods()}}</span>
   </div>
 </template>
 
 <script>
-
 export default {
-  name: 'App',
-  components: {
-    
-  }
-}
+  name: "App",
+  data() {
+    return {
+      firstName: "Harry",
+      lastName: "Potter",
+    };
+  },
+  computed: {
+    fullNameComputed() {
+      return `${this.firstName}  ${this.lastName}`;
+    },
+  },
+  methods: {
+    fullNameMethods() {
+      return `${this.firstName}  ${this.lastName}`;
+    },
+  },
+};
 </script>
 
 <style>
